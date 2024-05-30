b53d32f0eed68b5591e0eeef5214936b4294af6d
loveless-savage <titansofalgenia@gmail.com>
2024-05-30 13:19:40 -0600

    added gitignore

--- LaserFields/DumbFFT.m
+++ LaserFields/DumbFFT.m
@@ -1,39 +1,46 @@
 %% use rudimentary FFT to treat each subfrequency differently in space
 function [Ex,Ey,Ez,Bx,By,Bz]=DumbFFT(x,y,z,t)
-global Ep z0 tau;
+global Ep w0 tau;
 
-% frequency dependence
-N=100; % number of sub-frequencies in the eventual sum
-dw=8/tau/N; % frequency step
-wrange=(-N/2:N/2)*dw; % range of all frequencies of interest
+% frequency dependence: to make a time envelope of width sqrt(2)*tau, we
+% need a freqency envelope of width sqrt(2)/tau, which we can cover using
+% frequencies within +/-5x the envelope width.
+N=1000; % number of sub-frequencies in the eventual sum
+dw=5*2/tau/N; % frequency step
+wrange=dw*(-N/2:N/2); % range of sub-frequencies
 
-% calculate each frequency separately and accumulate them in field
-field = zeros(size(x));
-for n = 1:N
-    w = wrange(n);
+% to integrate frequencies we will accumulate field vectors
+Ex = zeros(size(z));
+Ey = zeros(size(z));
+Ez = zeros(size(z));
+Bx = zeros(size(z));
+By = zeros(size(z));
+Bz = zeros(size(z));
+
+% use the Singh model to build a gaussian focus for each frequency
+for n=1:N
+    deltaw = wrange(n);
+
     % intermediate constants
-    Z=z0+i*z; % coherence length w/ z phase
+    dz0=(1+deltaw)*w0^2/2; % focal length
+    Z=dz0+i*z; % coherence length w/ z phase
     rho2=x.^2+y.^2; % sideways radius
-    R=z+z0^2./z; % radial distance from focus
-    
+    R=z+dz0^2./z; % radial distance from focus
+
     % spatial gaussian envelope
     psi=z0./Z.*exp(-(1+w)*rho2./(2*Z));
-    % frequency envelope
-    wenv=exp(-(tau*w).^2/4);
-    % original plane wave for each frequency
-    plane=exp(i*(1+w).*z);
-    % scalar field strength in frequency space
-    wfield=Ep.*psi.*wenv.*plane;
-    
-    % now, we integrate across our given frequency range
-    field = field + wfield.*exp(-i*w*t)*dw;
-end
+    % frequency envelope;
+    wenv = exp(-(tau*deltaw)^2/2);
+    % plane wave itself
+    plane = exp(i*(1+deltaw)*(z-t)); % TODO: (z-t)?
+    % complete Singh model beam for this frequency
+    wfield = Ep*psi*wenv*plane*dw;
 
-field = field.*exp(-i*t);
-% vector elements
-Ex=real(field);
-Ey=real(x.*y./(2*Z.^2).*field);
-Ez=real(-i*x./Z.*field);
-Bx=1*Ey;
-By=1*Ex;
-Bz=1*real(-i*y./Z.*field);
+    % integrate vector components
+    Ex = Ex + real(wfield);
+    Ey = Ey + real(x.*y./(2*Z.^2).*wfield);
+    Ez = Ez + real(-i*x./Z.*wfield);
+    Bx = Ey;
+    By = Ex;
+    Bz = Bz + real(-i*y./Z.*wfield);
+end
