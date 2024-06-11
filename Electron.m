cd17bcf4c6d087a200bc738cf5571b74950cbdbd
loveless-savage <titansofalgenia@gmail.com>
2024-06-10 20:50:53 -0600

    sphere plotting improved

--- Electron.m
+++ Electron.m
@@ -9,16 +9,17 @@ tau=2*pi*12; % pulse duration in radians
 % for all available distortions, see the folder ./LaserFields/
 addpath("./LaserFields/")
 % which distortion are we using? For no distortion use @CircularEnvelope
-LaserField = @DumbFFT;
+LaserField = @Chirp;
+plottitlestr = join(['Distortion = ',func2str(LaserField)]);
 
 %% sample size & rendering parameters: these will be changed frequently!
 % # of electrons
-mmax=20;
+mmax=100;
 % polar plot resolution
-p_res=30;
+p_res=100;
 % frequency band-pass boundaries.  1=fundamental, 2=second harmonic.
-fmin=1.3;
-fmax=2.2;
+fmin=1.5; %1.3
+fmax=2.7; % 2.2
 % detector latitude for 2D polar plot
 latitude=pi/2;
 % which plots to show? default is just a 2D polar plot @ the equator
@@ -78,6 +79,7 @@ for n_e=1:mmax
 
     % find the electron trajectory
     [t,x,y,z,ux,uy,uz,ax,ay,az]=Trajectory(ti,tf,xi,yi,zi,pxi,pyi,pzi);
+    % show that trajectory
     % figure
     % plot(t/(2*pi),x/(2*pi),'b',t/(2*pi),y/(2*pi),'g',t/(2*pi),z/(2*pi),'r');
     
@@ -120,8 +122,11 @@ for n_e=1:mmax
 end
 
 %% polar plot of photon counts as a function of phi
-polarplot(p,Nt_tot,'g',p,Np_tot,'b','LineWidth',2)
-% frequency plot
+polarplot(p,Nt_tot,'g',p,Np_tot,'b','LineWidth',2);
+title(plottitlestr);
+legend('\theta polarization','\phi polarization','Location','bestoutside');
+
+%% frequency plot
 if showFrequencySpectrum
     nw = floor(length(nu)/4);
     plot(nu(1:nw),At_bp_tot(1:nw),'g',nu(1:nw),Ap_bp_tot(1:nw),'b');
@@ -134,8 +139,8 @@ if show3dFarField
     sph_res=p_res;
     [X,Y,Z]=sphere(sph_res);
     % initialize bins in a 2D array based on spherical coordinates
-    Nt_tot=zeros(sph_res+1,sph_res+1);
-    Np_tot=Nt_tot;
+    Nt_sph=zeros(sph_res+1,sph_res+1);
+    Np_sph=Nt_sph;
     % paint across all bins
     for k=1:sph_res+1 % theta (latitude)
         k
@@ -156,44 +161,51 @@ if show3dFarField
             [Ap_bp,nu]=Spectrum(td,Ap);
             % find photons in a certain frequency band
             for n_e=1:length(nu)
-                if nu(n_e)<fmin|nu(n_e)>fmax
+                if nu(n_e)<fmin||nu(n_e)>fmax
                     At_bp(n_e)=0;
                     Ap_bp(n_e)=0;
                 end
             end
             ft=trapz(nu,At_bp)*9.00e-4;
             fp=trapz(nu,Ap_bp)*9.00e-4;
-            Nt_tot(k,j)=ft/(1.55*(fmax+fmin)/2);
-            Np_tot(k,j)=fp/(1.55*(fmax+fmin)/2);
+            Nt_sph(k,j)=ft/(1.55*(fmax+fmin)/2);
+            Np_sph(k,j)=fp/(1.55*(fmax+fmin)/2);
         end
     end
-    figure;
+    % render bins
+    fig3d=figure;
+    fig3d.Position = [288 338 960 420];
+    % phi polarization
+    subplot(1,2,1);
     colormap([0 0 0;0 0 .25;0 0 .5;0 0 .75;0 0 1;0 .25 .75;0 .5 .5;0 .75 .25;0 1 0;.25 .75 0;.5 .5 0;.75 .25 .33;1 0 0;1 .25 .25;1 .5 .5;1 .75 .75;1 1 1]);
-    surf(X,Y,Z,Np_tot,'Edgecolor', 'none')
-    xlabel('x - axis')
-    ylabel('y - axis')
-    zlabel('z - axis')
-    shading interp
-    axis equal
-    alpha(.5)
-    hold on
+    surf(X,Y,Z,Np_sph,'Edgecolor', 'none');
+    title('\phi Polarization');
+    xlabel('x - axis');
+    ylabel('y - axis');
+    zlabel('z - axis');
+    shading interp;
+    axis equal;
+    alpha(.5);
+    hold on;
     zz= -1:1/10:1;
     [xx,yy,zz] = cylinder(w0*zz./z0);
     zz=(zz-.5)*2;
     c=zz*0;
-    surf(xx,yy,zz,c,'Edgecolor','white')
-    hold off
-    figure;
+    surf(xx,yy,zz,c,'Edgecolor','white'); % laser cone
+    % theta polarization
+    subplot(1,2,2);
     colormap([0 0 0;0 0 .25;0 0 .5;0 0 .75;0 0 1;0 .25 .75;0 .5 .5;0 .75 .25;0 1 0;.25 .75 0;.5 .5 0;.75 .25 .33;1 0 0;1 .25 .25;1 .5 .5;1 .75 .75;1 1 1]);
-    surf(X,Y,Z,Nt_tot,'Edgecolor', 'none')
-    xlabel('x - axis')
-    ylabel('y - axis')
-    zlabel('z - axis')
-    shading interp
-    axis equal
-    alpha(.5)
-    hold on
-    surf(xx,yy,zz,c,'Edgecolor','white')
-    hold off
+    surf(X,Y,Z,Nt_sph,'Edgecolor', 'none');
+    title('\theta Polarization');
+    xlabel('x - axis');
+    ylabel('y - axis');
+    zlabel('z - axis');
+    shading interp;
+    axis equal;
+    alpha(.5);
+    hold on;
+    surf(xx,yy,zz,c,'Edgecolor','white'); % laser cone
+    hold off;
+    sgtitle(plottitlestr); % overall title
 end
 
