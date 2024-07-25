4949fd4fe3ece1f41caddbcff1c60a4a3c46a9fd
loveless-savage <titansofalgenia@gmail.com>
2024-07-24 23:15:12 -0600

    separated some parts to a different repo

--- LaserFields/FieldCrossRender.m
+++
@@ -1,9 +0,0 @@
-%% this script takes a cross section of the beam & compares models
-global Ep w0 z0 tau;
-Ep=sqrt(1e18/2.146e18); % sqrt of intensity
-
-w0=2*pi*2; % beam waist
-z0=w0^2/2; % focal length
-dz=z0; % axial focal misalignment
-
-LaserField=@CircularEnvelope;
